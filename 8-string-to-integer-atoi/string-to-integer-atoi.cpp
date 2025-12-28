19e6a8204c4a919c86822564c9c1359991914fc0
RyanBahlawan <ryanbahlawan@gmail.com>
2025-12-27 20:08:45 -0500

    Time: 2 ms (18.67%) | Memory: 9.2 MB (21.76%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -6,24 +6,24 @@ public:
         }
         long long res = 0;
         int sign = 1, i = 0;
-        while(i<s.size() && s[i]== ' '){
+        while(i<s.size() && s[i]== ' '){//skip white spaces
             i++;
         }
-        if(i<s.size() && (s[i]=='-' || s[i]=='+')){
+        if(i<s.size() && (s[i]=='-' || s[i]=='+')){//check sign
             if(s[i]=='-'){
                 sign *= -1;
             }
             i++;
         }
-        while(i<s.size() && s[i]=='0'){
+        while(i<s.size() && s[i]=='0'){//skip leading 0's
             i++;
         }
-        while(i<s.size() && s[i]>='0' && s[i]<='9'){
+        while(i<s.size() && s[i]>='0' && s[i]<='9'){//add integers
             int digit = s[i]-'0';
             res = res*10+digit;
-            if(sign == 1 && res>INT_MAX){
+            if(sign == 1 && res>INT_MAX){//buffer overflow
                 return INT_MAX;
-            } else if(sign == -1 && -1*res<INT_MIN){
+            } else if(sign == -1 && -1*res<INT_MIN){//buffer overflow
                 return INT_MIN;
             }
             i++;
